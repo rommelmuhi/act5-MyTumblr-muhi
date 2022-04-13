620bd2477b7792c0792fbccbf2def3a660e5e500
Rommel Muhi <jromdaily@gmail.com>
2022-04-13 11:50:15 +0800

    added the login options like incorrect account, password and redirecting

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php 
+		session_start();
+
+		 $acc_username = "JhonRommel";
+		 $acc_password = "rommel1234";
+		 $acc_fullname = "Jhon Rommel N. Muhi";
+		 $acc_address = "Marinduque PH";
+
+		 $url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+		if(isset($_REQUEST['login_button']) === true){
+			if($_REQUEST['form_username'] != $acc_username){
+				header("Location: ".$url_add."?notexist");
+			}
+			elseif ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] != $acc_password){
+					header("Location: ".$url_add."?wrongpass");
+			}
+			elseif ($_REQUEST['form_username']== $acc_username && $_REQUEST['form_password'] == $acc_password){
+				header("Location: ".$url_add."?success");
+
+				$_SESSION['ses_username'] = $acc_username;
+				$_SESSION['ses_password'] = $acc_password;
+				$_SESSION['ses_fullname'] = $acc_fullname;
+				$_SESSION['ses_address'] = $acc_address;
+
+			}
+
+		}
+
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -24,12 +56,31 @@
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
 						
-						<form action="#" class="login-form">
+
+
+
+
+
+						<form method="POST" class="login-form">
+
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+
+		      			<?php 
+		      			if(isset($_REQUEST['notexist'])===true){
+										echo " <div class='alert alert-danger' role='alert'> Username does not exist.. </div> ";
+		      			} elseif (isset($_REQUEST['wrongpass'])===true){
+		      				echo " <div class='alert alert-warning' role='alert'> Incorrect password.. </div> ";
+		      			} elseif (isset($_REQUEST['success']) === true){
+		      				echo "<div class='alert alert-success' role='alert'> Redirecting.. </div>";
+		      				header ("Refresh: 5; url=account.php");
+		      			}
+
+		      			?>
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username" required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password" required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -43,9 +94,16 @@
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Get Started</button>
 	            </div>
 	          </form>
+
+
+
+
+
+
+
 	        </div>
 				</div>
 			</div>
